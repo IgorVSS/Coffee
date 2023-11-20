ffacb5a698c5018f6f5c05121f42ccd452a3bcab
Igor <bealmont@gmail.com>
2023-11-20 16:13:17 -0300

    first commit

---
+++ catalog/language/pt-br/extension/module/bestseller.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'Os mais pedidos';
+
+// Text
+$_['text_tax']      = 'Sem impostos:';
---
+++ catalog/language/pt-br/extension/module/featured.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'Para acompanhar';
+
+// Text
+$_['text_tax']      = 'Sem impostos:';
